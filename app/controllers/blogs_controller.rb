cfb939bddf0d1a7ca9331b0b12c526c94989f223
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-28 15:16:00 -0500

    added blog index page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,19 @@
 class BlogsController < ApplicationController
+
+    def index
+        @blogs = Blog.all
+    end
+
+    def show
+        @blog = Blog.find(params[:id])
+    end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        
+    end
+
 end
