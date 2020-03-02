769e897794ca9bf059d929fd4d96439b8ff58366
IdleScV <weschen1996@gmail.com>
2020-03-02 12:12:13 -0500

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :name, :password, :admin)
+        params.require(:user).permit(:username, :name, :password, :admin, :email)
     end
 
     def find_user
