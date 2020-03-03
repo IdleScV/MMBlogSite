61f3a1677191a6193f063b1673975666cfc014fb
rjwilson0531 <rjwilson0531@gmail.com>
2020-03-03 07:40:47 -0500

    reorg funs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,16 +8,6 @@ class BlogsController < ApplicationController
     def show
     end
 
-    def newcomment
-        Comment.create(content: params[:content], user_id: session[:user_id], blog: @blog )
-        redirect_to blog_path(@blog)
-    end
-
-    def newcommentlike
-       like =  CommentLike.create(comment_id: params[:id], user_id: session[:user_id])
-        redirect_to blog_path(like.comment.blog.id)
-    end
-
     def new
         @blog = Blog.new
     end
@@ -49,6 +39,16 @@ class BlogsController < ApplicationController
     def destroy
         @blog.destroy
         redirect_to blogs_path
+    end
+
+    def newcomment
+        Comment.create(content: params[:content], user_id: session[:user_id], blog: @blog )
+        redirect_to blog_path(@blog)
+    end
+
+    def newcommentlike
+       like =  CommentLike.create(comment_id: params[:id], user_id: session[:user_id])
+        redirect_to blog_path(like.comment.blog.id)
     end
     
     def is_liked?
