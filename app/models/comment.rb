34b5f24f487ae3d81643c7a008681ebb0ee858fa
IdleScV <weschen1996@gmail.com>
2020-03-03 16:34:24 -0500

    Revert "Merge branch '3_3_2020' of https://github.com/IdleScV/MMBlogSite into 3_3_2020"
    This reverts commit 31e16c018415617ec026d5b3abc37f231b57d7a7, reversing
    changes made to a98b9faa10641d50adbbf3d828d5128997cf6105.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,4 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :blog
     has_many :comment_likes
-
-    validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     has_many :comment_likes
-    #username, email, name password
-    validates :username, presence: true
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    
+
+       
     def like(blog)
         Like.create(blog: blog, user_id: self.id)
     end
