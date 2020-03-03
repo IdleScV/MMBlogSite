ae1abbf101d150f49c73cda398e7dda9dac13ee8
IdleScV <weschen1996@gmail.com>
2020-03-02 20:25:59 -0500

    html

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :blogs, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
-
+    
        
     def like(blog)
         Like.create(blog: blog, user_id: self.id)
